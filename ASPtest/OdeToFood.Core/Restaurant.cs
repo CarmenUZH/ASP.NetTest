1e4c66f306561cef07cee21e03c391dcd5a0c1a6
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-06-28 09:31:49 +0200

    Restaurants

---
+++ ASPtest/OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Core //New class for Restaurants
+{
+    public enum FoodType
+    {
+        None,
+        Mexican,
+        French,
+        Garbage
+    }
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+    }
+}
