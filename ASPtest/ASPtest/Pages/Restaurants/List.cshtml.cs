dda885d941b5755d5d7cc9bd4f02c9fe9e949d17
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-06-28 09:27:51 +0200

    Continuing work

--- ASPtest/ASPtest/Pages/Restaurants/List.cshtml.cs
+++ ASPtest/ASPtest/Pages/Restaurants/List.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ASPtest.Pages.Restaurants
 {
-    public class ListModel : PageModel
+    public class ListModel : PageModel //Use pagemodel to inject services that will give me access to data and then use it in List.cshtml
     {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+        public string NewMessage { get; set; }
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public void OnGet()
         {
+            Message = "My Restaurant";
+            NewMessage = config["Message"];
         }
+        //Page Model is responsible for fetching information
     }
 }
