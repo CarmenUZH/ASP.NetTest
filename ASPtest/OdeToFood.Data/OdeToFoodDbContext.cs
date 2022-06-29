8b825ca1db33ac6c65528c791ea05b80b4186c84
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-06-29 14:03:17 +0200

    Database start!
    Be sure to install correct versions & cd into correct directories

---
+++ ASPtest/OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+     public class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurant { get; set; } //DbSet so that i not only query but also insert update and delete from database
+        //Can have dozens of Entities
+    }
+}
