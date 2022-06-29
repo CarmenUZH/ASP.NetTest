7a1e26334d83dafac06b7b778bfd5fb7948e294f
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-06-29 14:39:46 +0200

    what

---
+++ ASPtest/OdeToFood.Data/Migrations/20220629123010_initialcreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+//Dont fuckint trust Databases
+
+namespace OdeToFood.Data.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable( //Creates tables for me
+                name: "Restaurant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(280)", maxLength: 280, nullable: true),
+                    Food = table.Column<int>(type: "int", nullable: false),
+                    Deaths = table.Column<int>(type: "int", nullable: false),
+                    Animatronics = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurant");
+        }
+    }
+}
