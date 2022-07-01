d24689151607ff65f19a7941431f66c383de5135
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-01 13:21:37 +0200

    Uuuuuuuuuh?
    I mean... it works?
    But should it?
    Im not sure.

--- ASPtest/ASPtest/Program.cs
+++ ASPtest/ASPtest/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OdeToFood.Data;
+using System;
 
 namespace ASPtest
 {
@@ -7,7 +11,21 @@ namespace ASPtest
     {
         public static void Main(string[] args)//Like Java, needs to be there
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //Step between hosting and running
+            MigrateDatabase(host);
+                
+                host.Run();
+        }
+
+        private static void MigrateDatabase(IHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<OdeToFoodDbContext>();
+                db.Database.Migrate();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- ASPtest/ASPtest/Startup.cs
+++ ASPtest/ASPtest/Startup.cs
@@ -45,7 +45,6 @@ namespace ASPtest
             else
             {
                 app.UseExceptionHandler("/Error"); //Userfriendly Error handler 
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
