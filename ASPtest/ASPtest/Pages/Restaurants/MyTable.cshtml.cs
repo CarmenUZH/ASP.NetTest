f3e150df85f4045854f61a48a83d3a08f1f43f27
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-18 13:22:30 +0200

    Editable Table!

---
+++ ASPtest/ASPtest/Pages/Restaurants/MyTable.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Threading.Tasks;
+
+namespace ASPtest.Pages.Restaurants
+{
+    public class MyTableModel : PageModel
+    {
+      
+
+            public async Task<JsonResult> OnPost(String txt, bool chk)
+            {
+
+                await Task.Delay(500);
+
+                // insert a break point to check 
+                // the String txt and bool chk 
+                // parameters received above 
+                // your database code can be written 
+                // to process these values - an ID 
+                // would be required too 
+                return new JsonResult("OK");
+
+            }
+
+        }
+
+    }
+
+
