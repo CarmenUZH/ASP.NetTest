ca65ac787304d619fe4f61a231ad1253bd1c3308
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-18 15:48:17 +0200

    Doesnt work without a database
    Hmm

--- ASPtest/ASPtest/Pages/Restaurants/MyTable.cshtml.cs
+++ ASPtest/ASPtest/Pages/Restaurants/MyTable.cshtml.cs
@@ -1,31 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ASPtest.Pages.Restaurants
 {
     public class MyTableModel : PageModel
     {
+        public List<string> Pizzas = new List<string>();  //Important to make this get; set; so that you always get it
       
+        public IActionResult OnGet() //strictly an input model
+        {
+            return Page();
+        }
+        public async Task<JsonResult> OnPost(String txt, bool chk)
+        {
+            await Task.Delay(500);
+            Pizzas.Add(txt);
 
-            public async Task<JsonResult> OnPost(String txt, bool chk)
-            {
-
-                await Task.Delay(500);
-
-                // insert a break point to check 
-                // the String txt and bool chk 
-                // parameters received above 
-                // your database code can be written 
-                // to process these values - an ID 
-                // would be required too 
-                return new JsonResult("OK");
-
-            }
-
+            // insert a break point to check 
+            // the String txt and bool chk 
+            // parameters received above 
+            // your database code can be written 
+            // to process these values - an ID 
+            // would be required too 
+            return new JsonResult("OK");
 
         }
+
+
+    }
 
     }
 
